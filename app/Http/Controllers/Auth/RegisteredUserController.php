6b95f25dce14a96c4be04f23f7bf44e21c58a5f7
WalidElmiloudi <elwalid38@gmail.com>
2026-04-19 17:09:41 +0100

    feat: enhance registration process with role selection and error handling

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,14 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'role' => ['required','string'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'role' => $request->role,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
@@ -46,6 +48,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('home', absolute: false));
     }
 }
--- resources/views/forms/register.blade.php
+++ resources/views/forms/register.blade.php
@@ -1,16 +1,25 @@
       <form
           class="h-[85%] w-[90%] 2xl:w-[80%] 2xl:h-[70%] flex flex-col items-center rounded-lg bg-red-100/20 p-2 gap-2 2xl:gap-3"
           action="{{ route('register') }}" method="post">
+          @if ($errors->any())
+              <div class="alert alert-danger">
+                  <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                  </ul>
+              </div>
+          @endif
           <label class="text-xl self-start lg:text-2xl" for="name"> Full Name :</label>
           <input class="h-10 rounded-lg w-full bg-white pl-2 text-xl lg:text-2xl lg:py-2" type="text" name="name"
               placeholder="Jean Do">
           <label class="text-xl self-start lg:text-2xl" for="email"> Email :</label>
-          <input class="h-10 rounded-lg w-full bg-white pl-2 text-lg lg:text-xl lg:py-2" type="email" name="name"
+          <input class="h-10 rounded-lg w-full bg-white pl-2 text-lg lg:text-xl lg:py-2" type="email" name="email"
               placeholder="JeanDo@example.domain">
           <label class="text-xl self-start lg:text-2xl" for="role">Choose Your Role :</label>
           <select class="h-10 rounded-lg w-full bg-white pl-2 text-xl lg:text-2xl lg:py-2 2xl:text-xl" name="role">
-              <option value="Client">Client</option>
-              <option value="Vendor">Vendor</option>
+              <option value="client">Client</option>
+              <option value="vendor">Vendor</option>
           </select>
           <label class="text-xl self-start lg:text-2xl" for="password"> Password :</label>
           <input class="h-10 rounded-lg w-full bg-white pl-2 text-xl lg:text-2xl lg:py-2" type="password"
