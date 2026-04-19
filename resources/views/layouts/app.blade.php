ce8a676f16c274f3291166e8d571c1747f2318c3
WalidElmiloudi <elwalid38@gmail.com>
2026-04-19 15:18:33 +0100

    Add main layout and partials for header and navbar
    - Created app.blade.php for the main application layout.
    - Added header.blade.php for the top header section including logo and search functionality.
    - Implemented navbar.blade.php for responsive navigation with icons and links.
    - Established a directory structure documentation in structure.text for better organization of views.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css" />
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/thin/style.css" />
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
+  <title>{{config('app.name')}} | @yield('page')</title>
+</head>
+
+<body class="font-[Open_Sans] relative">
+   @include('partials.header')
+   @include('partials.navbar')
+   @yield('content')
+</body>
+
+</html>
